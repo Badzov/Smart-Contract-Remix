65e5d377759fddf6c076609aba7deedd23144614
Badzov <vule.badza@gmail.com>
2026-03-17 16:10:24 +0100

    Dodata funkcija finalize.

--- contracts/LicenseContract.sol
+++ contracts/LicenseContract.sol
@@ -65,4 +65,20 @@ contract LicenseContract {
         refundApproved = false;
     }
 
+    function finalize() public {
+        require(state != State.Completed, "Contract already completed");
+        require(buyer != address(0), "License not purchased yet");
+        require(block.timestamp >= purchaseTime + 1 minutes, "Too early to finalize"); /* Ovde treba da stoji 7 days, ali radi testiranja smo stavili 1 minutes */
+
+        state = State.Completed;
+
+        if (refundRequested && refundApproved) {
+            (bool success, ) = payable(buyer).call{value: address(this).balance}("");
+            require(success, "Transfer to buyer failed");
+        } else {
+            (bool success, ) = payable(owner).call{value: address(this).balance}("");
+            require(success, "Transfer to owner failed");
+        }
+    }
+
 }
